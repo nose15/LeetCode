ecd291a079077998362e224dc5741ec6814c0603
lukasz <lukol12321@gmail.com>
2024-05-20 10:03:26 +0200

    Task 167 - 2

--- 167-two-sum-2.cpp
+++ 167-two-sum-2.cpp
@@ -24,3 +24,28 @@ public:
         return {i + 1, j + 1};
     }
 };
+
+// But this has a lower complexity because it's only one loop and the first one 
+// although it doesnt have nested loops that would have time complexity O(n), it// acts like it
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0;
+        int j = numbers.size() - 1;
+        
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum > target) {
+                j--;
+            }
+            else if (sum < target) {
+                i++;
+            }
+            else {
+                return {i + 1, j + 1};
+            }
+        }
+
+        return {i + 1, j + 1};
+    }
+};
