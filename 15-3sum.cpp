aeb5dc2b71d43a3926a084cbcea5057a48d0f880
lukasz <lukol12321@gmail.com>
2024-05-21 20:07:57 +0200

    Task 15

---
+++ 15-3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1, k = nums.size() - 1;
+
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                }
+                else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                }
+                else {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
