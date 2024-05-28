4745f247ce84e8c81c31d97f6ed245335a4fa576
lukasz <lukol12321@gmail.com>
2024-05-28 21:25:29 +0200

    Task 739 - 2

--- 739-daily-temperatures.cpp
+++ 739-daily-temperatures.cpp
@@ -1,3 +1,26 @@
+// My original solution, ditched it because the memory efficiency was bad. Turns out it's the best one lol.
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+
+        stack<int> indecesToMatch;
+        vector<int> res(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            while (!indecesToMatch.empty() && temperatures[indecesToMatch.top()] < temperatures[i]) {
+                int index = indecesToMatch.top();
+                res[index] = i - index;
+                indecesToMatch.pop();
+            }
+
+            indecesToMatch.push(i); 
+        }
+
+        return res;
+    }
+};
+
 // My solution (pretty fast but not memory efficient)
 class Solution {
 public:
